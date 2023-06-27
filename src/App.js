055f3410cfa67edca617a9e2ad00c8d7f0f16c53
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-06-27 15:51:35 +0530

    27-6-matireal-ui-drawer

--- src/App.js
+++ src/App.js
@@ -1,53 +1,61 @@
 import './App.css';
-import Footer from './component/Footer';
-import Header from './component/Header';
+import Footer from './user/component/Footer';
+import Header from './user/component/Header';
 import {Route, Routes } from 'react-router-dom';
-import Home from './container/Home';
-import About from './container/About';
-import Contact from './container/Contact';
-import Appointment from './container/Appointment';
-import Departments from './container/Departments';
-import Doctors from './container/Doctors';
-import Doctor from './container/Doctor';
-import VisitingDoctor from './container/VisitingDoctor';
-import NotFound from './container/Notfound';
-import Auth from './container/Auth';
-import Extra from './container/Extra';
+import Home from './user/container/Home';
+import About from './user/container/About';
+import Contact from './user/container/Contact';
+import Appointment from './user/container/Appointment';
+import Departments from './user/container/Departments';
+import Doctors from './user/container/Doctors';
+import Doctor from './user/container/Doctor';
+import VisitingDoctor from './user/container/VisitingDoctor';
+import NotFound from './user/container/Notfound';
+import Auth from './user/container/Auth';
+import Extra from './user/container/Extra';
 // import Auth1 from './container/Auth1';
-import Validation from './container/Validation';
-import Dropdown from './container/Dropdown';
-
-
-
+import Validation from './user/container/Validation';
+import Dropdown from './user/container/Dropdown';
+import DepartmentsDrawer from './admin/container/DepartmentsDrawer';
+import DoctorDrawer from './admin/container/DoctorDrawer'
+import MedicineDrawer from './admin/container/MedicineDrawer'
+import AppointmentDrawer from './admin/container/AppointmentDrawer'
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Routes>
-        <Route  path='/' element={<Home />}/>
+        {/* <Route  path='/' element={<Home />}/>
         <Route  path='/departments' element={<Departments/>} />
         <Route  path='/doctors' element={<Doctors />}/>
         <Route  path='/about' element={<About />}/>
         <Route  path='/contact' element={<Contact />}/>
-        <Route path='/appointment' element={<Appointment />} />
-        {/* <Route path='/doctor/:id' element={<Doctor />} />
-        <Route path='/doctor/visiting_doctor' element={<VisitingDoctor />} /> */}
+        <Route path='/appointment' element={<Appointment />} /> */}
+        // {/* <Route path='/doctor/:id' element={<Doctor />} />*/}
+        // {/* <Route path='/doctor/visiting_doctor' element={<VisitingDoctor />} />   */}
 
-        <Route path='/doctor/'>
+        {/* <Route path='/doctor/'>
         <Route path=':id' element={<Doctor />} />
         <Route path='visiting_doctor' element={<VisitingDoctor />} />
-        </Route>
+        </Route> */}
 
-        <Route path='*' element={<NotFound />} />
-        <Route path='/auth'  element={<Auth />}/>
-        {/* <Route path='/auth' element={<Auth1 />} /> */}
-        <Route path='/extra' element={<Extra />} />
-        <Route path='/validation' element={<Validation />} />
-        <Route path='/dropdown' element={<Dropdown />} />
+        {/* <Route path='*' element={<NotFound />} />
+        <Route path='/auth'  element={<Auth />}/> */}
+      //  {/* <Route path='/auth' element={<Auth1 />} /> */}
+        {/* <Route path='/extra' element={<Extra />} />
+        <Route path='/validation' element={<Validation />} /> */}
+        <Route path='/dropdown' element={<Dropdown/>} />
+        <Route path='/department_drawer' element={<DepartmentsDrawer />} />
+        <Route path='/doctor_drawer' element={<DoctorDrawer />}/>
+        <Route path='/medicine_drawer' element={<MedicineDrawer />}/>
+        <Route path='/appointment_drawer' element={<AppointmentDrawer />}/>
       </Routes>
-      <Footer /> 
+      {/* <Footer /> */}
+  
     </>
+
+   
 
   );
 }
